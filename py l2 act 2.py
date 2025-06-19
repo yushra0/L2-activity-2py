51dd51e95277d77b5277d3a0af00f36b9a2d0c59
yushra0 <zayaanyushra@gmail.com>
2025-06-19 21:00:44 +0600

    Add files via upload

---
+++ py l2 act 2.py
@@ -0,0 +1,21 @@
+num1 = 45
+num2 = 3
+
+print ("Number 1", num1)
+print ("Number 2", num2)
+print ("Addition:", num1+num2)
+print ("Difference :", num1-num2)
+print("Product :", num1*num2)
+print ("Division :", num1/num2)
+print("Floor Division :", num1//num2)
+print("Modulus Operation :", num1%num2)
+print("Square:", num2**2)
+print ("Square Root:", num1**0.5)
+
+print ("Equal ?", num1==num2) 
+print ("Number 1 greater?", num1>num2 )
+print ("Number 2 greater?" , num1<num2)
+print("Not Equal ?", num1!=num2)
+
+result = num1/2+num2**2+10
+print ("Result of given equation is:", result)
